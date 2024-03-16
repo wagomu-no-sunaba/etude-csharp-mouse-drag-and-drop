8a3b9ac86493e60ee07c26259da7496fa9a884ab
staticWagomU <to6wa17@gmail.com>
2024-03-16 22:13:05 +0900

    プロジェクト ファイルを追加します。

---
+++ etude-csharp-mouse-drag-and-drop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace etude_csharp_mouse_drag_and_drop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
