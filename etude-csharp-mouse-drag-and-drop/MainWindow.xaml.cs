5202319491721b3378e0d3964a4e09ded95f9f40
staticWagomU <to6wa17@gmail.com>
2024-03-16 22:55:49 +0900

    feat: drag and drop

--- etude-csharp-mouse-drag-and-drop/MainWindow.xaml.cs
+++ etude-csharp-mouse-drag-and-drop/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Win32;
+using Windows.Win32.UI.Input.KeyboardAndMouse;
+using Windows.Win32.Foundation;
 
 namespace etude_csharp_mouse_drag_and_drop
 {
@@ -19,6 +22,45 @@ namespace etude_csharp_mouse_drag_and_drop
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var startX = 100;
+            var startY = 100;
+            var endX = 500;
+            var endY = 500;
+
+            var duration = 1000; // ミリ秒
+            MOUSE_EVENT_FLAGS MouseEventLeftDown = (MOUSE_EVENT_FLAGS)0x0002;
+            MOUSE_EVENT_FLAGS MouseEventLeftUp = (MOUSE_EVENT_FLAGS)0x0004;
+
+
+        var mouseThread = new Thread(() =>
+            {
+                DateTime startTime = DateTime.Now;
+                PInvoke.SetCursorPos(startX, startY);
+                PInvoke.mouse_event(MouseEventLeftDown, 0, 0, 0, 0);
+                Thread.Sleep(10);
+                while (true)
+                {
+                    var elapsed = (DateTime.Now - startTime).TotalMilliseconds;
+                    var t = Math.Min(elapsed / duration, 1.0);
+
+                    var currentX = (int)Math.Round(startX + (endX - startX) * t);
+                    var currentY = (int)Math.Round(startY + (endY - startY) * t);
+
+                    PInvoke.SetCursorPos(currentX, currentY);
+
+                    if (t >= 1.0) break;
+
+                    Thread.Sleep(10); // 10ミリ秒ごとに更新
+                }
+                PInvoke.mouse_event(MouseEventLeftUp, 0, 0, 0, 0);
+
+            });
+
+            mouseThread.Start();
         }
     }
 }
